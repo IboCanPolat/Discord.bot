a9a335f934dc8f5b4698f5990857bde8c5f09ed3
IboCanPolat <72688318+IboCanPolat@users.noreply.github.com>
2020-10-11 13:05:40 +0300

    Create bot-dm-görme.js

---
+++ komutlar/bot-dm-görme.js
@@ -0,0 +1,11 @@
+client.on("message", message => {
+    const dmchannel = client.channels.find("name", "dm-log");
+    if (message.channel.type === "dm") {
+        if (message.author.bot) return;
+        dmchannel.sendMessage("", {embed: {
+            color: 3447003,
+            title: `Gönderen: ${message.author.tag}`,
+            description: `Bota Özelden Gönderilen DM: ${message.content}` //Deunes Code
+        }})
+    }
+});
